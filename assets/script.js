320019f4c1dffb05c709bd448b41c60b8209114c
Viren <viren15152@gmail.com>
2023-11-08 18:36:44 +0000

    Latest Updates to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
     //I have created this function so that it will load recently searched cities from local storage.
 
     function loadRecentCities() {
-        let loadRecentCities = JOSN.parse(localStorage.getItem("cities"));  
+        let loadRecentCities = JSON.parse(localStorage.getItem("cities"));  
 
         if (loadRecentCities) {
             cities = recentCities;
@@ -73,7 +73,7 @@ function getCity() {
 //This function will allow me to search the API for the chosen City
 function search() {
 
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=0ecd6941b13b3aa52fd2d06126b3ff33";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?=" + city + "&units=imperial&appid=";
     let coords = [];
 
     $.ajax({
@@ -107,5 +107,101 @@ function search() {
     }).fail(function(){
         alert("Could not obtain data")
     });
+
+    function getUV(lat, lon) {
+
+
+        $.ajax({
+            url: "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + "&units=imperial&appid=",
+            method: "GET",
+        }).then(function (response) {
+
+            let uvIndex = response.current.uvi;
+            $("#uv-index").text("UV Index:" + " " + uvIndex);
+            if (uvIndex >= 8) {
+                $("uv-index").css("color", "red");
+            } else if (uvIndex > 4 && uvIndex < 8) {
+                $("uv-index").css("color", "purple");
+            } else {
+                $("uv-index").css("color", "orange");
+            }
+            let cityHigh = response.daily[0].temp.max;
+            $("#high").text("Expected high (F): " + " " + cityHigh);
+
+            //These are my variables for temperature 
+            let day1temp = response.daily[1].temp.max;
+            let day2temp = response.daily[2].temp.max;
+            let day3temp = response.daily[3].temp.max;
+            let day4temp = response.daily[4].temp.max;
+            let day5temp = response.daily[5].temp.max;
+            //These are my variables for humidity   
+            let day1hum = response.daily[1].humidity;
+            let day2hum = response.daily[2].humidity;
+            let day3hum = response.daily[3].humidity;
+            let day4hum = response.daily[4].humidity;
+            let day5hum = response.daily[5].humidity;
+            //These are my icon variables for forecast weather
+            let icon1 = response.daily[1].weather[0].icon
+            let icon2 = response.daily[2].weather[0].icon
+            let icon3 = response.daily[3].weather[0].icon
+            let icon4 = response.daily[4].weather[0].icon
+            let icon5 = response.daily[5].weather[0].icon
+
+            $("#temp1").text("Temp(F):" + " " + day1temp.toFixed(1));
+            $("#temp2").text("Temp(F):" + " " + day2temp.toFixed(1));
+            $("#temp3").text("Temp(F):" + " " + day3temp.toFixed(1));
+            $("#temp4").text("Temp(F):" + " " + day4temp.toFixed(1));
+            $("#temp5").text("Temp(F):" + " " + day5temp.toFixed(1));
+
+            $("#hum1").text("Hum:" + " " + day1hum + "%");
+            $("#hum2").text("Hum:" + " " + day2hum + "%");
+            $("#hum3").text("Hum:" + " " + day3hum + "%");
+            $("#hum4").text("Hum:" + " " + day4hum + "%");
+            $("#hum5").text("Hum:" + " " + day5hum + "%");
+
+            $("#icon1").html(
+                `<img src=http://openweathermap.org/img/wn/${icon1}@2x.png>`
+            );
+            $("#icon2").html(
+                `<img src=http://openweathermap.org/img/wn/${icon2}@2x.png>`
+            );
+            $("#icon3").html(
+                `<img src=http://openweathermap.org/img/wn/${icon3}@2x.png>`
+            );
+            $("#icon4").html(
+                `<img src=http://openweathermap.org/img/wn/${icon5}@2x.png>`
+            );
+            $("#icon5").html(
+                `<img src=http://openweathermap.org/img/wn/${icon5}@2x.png>`
+            );
+        });
+    }
+}
+
+//This function will allow me to render recently searched cities to my page at
+
+function listCities() {
+    $("#cityList").text("");
+    cities.forEach((city) => {
+        $("#cityList").prepend("<tr><td>" + city + "</td></tr>");
+    });
+}
+
+//This is an event handler for recently searched cities in my table 
+listCities();
+$(document).on("click", "td", (e) => {
+    e.preventDefault();
+    let listedCity = $(e.target).text();
+    city = listedCity;
+    search();
+});
+
+//My final section of code in my script.js is an event handler for my clear/reset button in my HTML
+$("#clr-btn").click(() => {
+    localStorage.removeItem("cities");
+    loadRecentCities();
+    listCities();
+});
+});
 
 
